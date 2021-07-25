ea401c336238040706a22f35ce12a0ed59140f91
Annalena <annalena.bloch@tum.de>
2021-07-25 20:58:05 +0200

    small fix

--- src/transopt.h
+++ src/transopt.h
@@ -4,10 +4,9 @@
 constexpr double DEPTH = 1.7;
 
 double* optimizeTranslation(HandModel& hands,
-						 double* translation,
-						 Hand hand,
-						 std::array<std::array<double, NUM_OPENPOSE_KEYPOINTS * 3>, 2> keypoints,
-						 SimpleCamera& camera,
-						 int frame_width,
-						 int frame_height);
+						    Hand hand,
+						    std::array<std::array<double, NUM_OPENPOSE_KEYPOINTS * 3>, 2> keypoints,
+						    SimpleCamera& camera,
+						    int frame_width,
+						    int frame_height);
 void testTransOptimization();
